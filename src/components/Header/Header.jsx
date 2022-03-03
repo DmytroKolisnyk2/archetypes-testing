c751b652c61650fb6c10ab2328b840cf02511974
unknown <you@example.com>
2022-03-03 17:38:52 +0200

    components/navigations

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Navigation from "../Navigation/Navigation.jsx";
-
+import NavigationStyles from "../Navigation/Navigation.module.scss"
 const Header = () => (
-  <header>
+  <header className={NavigationStyles.header}>
     <Navigation />
   </header>
 );
