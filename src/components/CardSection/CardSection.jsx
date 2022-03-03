f71b8571d2621aabf4b7b9fe3964e4f232949c70
Igoryakib <Igoryakib@gmail.com>
2022-03-03 19:53:28 +0200

    added adaptive & doc

--- src/components/CardSection/CardSection.jsx
+++ src/components/CardSection/CardSection.jsx
@@ -12,22 +12,28 @@ const CardSection = ({
   color,
   smallRectangular,
 }) => {
-const addClass = (smallRectangular, type) => {
-  return classNames({
-    [styles.card_smallRectangular]: smallRectangular === true && type === "card",
-    [styles.content_smallRectangular]: smallRectangular === true && type === "content",
-    [styles.title_smallRectangular]: smallRectangular === true && type === "title",
-    [styles.wrapper_smallRectangular]: smallRectangular === true && type === "wrapper",
-    [styles.image_smallRectangular]: smallRectangular === true && type === "image",
-    [styles.description_smallRectangular]: smallRectangular === true && type === "description",
-    [styles.card]: smallRectangular !== true && type === "card",
-    [styles.content]: smallRectangular !== true && type === "content",
-    [styles.title]: smallRectangular !== true && type === "title",
-    [styles.wrapper]: smallRectangular !== true && type === "wrapper",
-    [styles.image]: smallRectangular !== true && type === "image",
-    [styles.description]: smallRectangular !== true && type === "description",
-  })
-};
+  const addClass = (smallRectangular, type) => {
+    return classNames({
+      [styles.card_smallRectangular]:
+        smallRectangular === true && type === "card",
+      [styles.content_smallRectangular]:
+        smallRectangular === true && type === "content",
+      [styles.title_smallRectangular]:
+        smallRectangular === true && type === "title",
+      [styles.wrapper_smallRectangular]:
+        smallRectangular === true && type === "wrapper",
+      [styles.image_smallRectangular]:
+        smallRectangular === true && type === "image",
+      [styles.description_smallRectangular]:
+        smallRectangular === true && type === "description",
+      [styles.card]: smallRectangular !== true && type === "card",
+      [styles.content]: smallRectangular !== true && type === "content",
+      [styles.title]: smallRectangular !== true && type === "title",
+      [styles.wrapper]: smallRectangular !== true && type === "wrapper",
+      [styles.image]: smallRectangular !== true && type === "image",
+      [styles.description]: smallRectangular !== true && type === "description",
+    });
+  };
   return (
     <div className={addClass(smallRectangular, "card")}>
       {smallRectangular ? (
@@ -35,13 +41,19 @@ const addClass = (smallRectangular, type) => {
       ) : (
         <Rectangle color={color} />
       )}
-        <div className={addClass(smallRectangular, "content")}>
-          <h2 className={addClass(smallRectangular, "title")}>{title}</h2>
-          <div className={addClass(smallRectangular, "wrapper")}>
-            <img className={addClass(smallRectangular, "image")} src={cardImg} alt="card_img" />
-            <p className={addClass(smallRectangular, "description")}>{description}</p>
-          </div>
+      <div className={addClass(smallRectangular, "content")}>
+        <h2 className={addClass(smallRectangular, "title")}>{title}</h2>
+        <div className={addClass(smallRectangular, "wrapper")}>
+          <img
+            className={addClass(smallRectangular, "image")}
+            src={cardImg}
+            alt="card_img"
+          />
+          <p className={addClass(smallRectangular, "description")}>
+            {description}
+          </p>
         </div>
+      </div>
     </div>
   );
 };
@@ -57,5 +69,25 @@ CardSection.propTypes = {
 CardSection.defaultProps = {
   smallRectangular: false,
 };
+
+// Дока
+// Щоб викликати маленьку крім основних пропів потрібно прокинути smallRectangular. Решта все як завжди
+// Виклик великої трапеції(картки)
+{
+  /* <CardSection
+color="green"
+title="Реализовать свой потенциал"
+description="Архетипы помогают обнаружить свои нереализованные ценности и разблокировать к ним доступ. Интеграция ценностей своих архетипов в жизнь помогают проживать ее на полную и чувствовать удволетворенность."
+cardImg={CardImg}
+/> */
+}
+// Виклик малої трапеції(картки)
+{/* <CardSection
+smallRectangular
+color="purple"
+title="Время"
+description="10 минут"
+cardImg={smallImg}
+/> */}
 
 export default CardSection;
