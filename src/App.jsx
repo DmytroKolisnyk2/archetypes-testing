916d25584a17b54ecb6fac68a6a7111f7c8aed05
unknown <you@example.com>
2022-03-03 16:38:36 +0200

    component/navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import { Routes, Route } from "react-router-dom";
 import path from "./services/routerPath.json";
-
+import Header from "./components/Header/Header"
 import TestPage from "./pages/TestPage/TestPage";
 
 
 class App extends Component {
   render() {
     return (
+      <>
+      <Header/>
+
       <Routes>
         <Route path={path.test} element={<TestPage />} />
       </Routes>
+    </>
     );
   }
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navigation from "../Navigation/Navigation.jsx";
+
+const Header = () => (
+  <header>
+    <Navigation />
+  </header>
+);
+
+export default Header;
