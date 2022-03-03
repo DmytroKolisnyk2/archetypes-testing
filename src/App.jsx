323ebd633b6255c3a5de52a76d9d8cc49dd76791
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-03-03 14:00:23 +0200

    add closable value

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import path from "./services/routerPath.json";
 
 import TestPage from "./pages/TestPage/TestPage";
-import ArchetypesCard from "./components/ArchetypesCard/ArchetypesCard";
+
 
 class App extends Component {
   render() {
     return (
-      <ArchetypesCard>
-        hjh
-      </ArchetypesCard>
+      <Routes>
+        <Route path={path.test} element={<TestPage />} />
+      </Routes>
     );
   }
 }
--- src/components/ArchetypesCard/ArchetypesCard.jsx
+++ src/components/ArchetypesCard/ArchetypesCard.jsx
@@ -2,61 +2,97 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./ArchetypesCard.module.scss";
 import Arrow from "../../images/icons/upArrow.svg";
-import Ruler from "../../images/ruler.jpg";
 
-export default function ArchetypesCard() {
+export default function ArchetypesCard({
+  motivationText,
+  fearText,
+  strongSideText,
+  weakSideText,
+  strategyText,
+  headline,
+  openHeadline,
+  image,
+  firstParagraph,
+  secondParagraph,
+}) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <article className={`${styles.card} ${isOpen && styles.card_open}`}>
-      <div className={styles.captionWrapper}>
-        <h2 className={styles.caption}>Второй Архетип</h2>
-        <button className={styles.button} type="button" onClick={() => setIsOpen(!isOpen)}>
-          <img src={Arrow} alt="arrow" className={styles.arrow} />
-        </button>
-      </div>
+    <article
+      className={`${styles.card} ${
+        (isOpen || !openHeadline) && styles.card_open
+      } ${!openHeadline&&styles.card_notClosable}`}
+    >
+      {openHeadline && (
+        <div className={styles.captionWrapper}>
+          <h2 className={styles.caption}>{openHeadline}</h2>
+          <button className={styles.button} type="button" onClick={() => setIsOpen(!isOpen)}>
+            <img src={Arrow} alt="arrow" className={styles.arrow} />
+          </button>
+        </div>
+      )}
       <div className={styles.cardWrapper}>
         <div className={styles.contentWrapper}>
-          <img className={styles.image} src={Ruler} alt="ruler" />
+          <img className={styles.image} src={image} alt="ruler" />
           <div className={styles.textWrapper}>
-            <h2 className={styles.caption}>Правитель</h2>
+            <h2 className={styles.caption}>{headline}</h2>
             <p className={styles.characteristic}>
-              <span>Мотивация:</span> бути успішним
+              <span>Мотивация:</span> {motivationText}
             </p>
             <p className={styles.characteristic}>
-              <span>Страх:</span> бути успішним
+              <span>Страх:</span> {fearText}
             </p>
             <p className={styles.characteristic}>
-              <span>Cильные стороны:</span> бути успішним
+              <span>Cильные стороны:</span> {strongSideText}
             </p>
             <p className={styles.characteristic}>
-              <span>Слабые стороны:</span> бути успішним
+              <span>Слабые стороны:</span> {weakSideText}
             </p>
             <p className={styles.characteristic}>
-              <span>Стратегия:</span> управлять другими, через авторитет и достигать результата{" "}
+              <span>Стратегия:</span> {strategyText}
             </p>
           </div>
         </div>
         <div className={styles.paragraphWrapper}>
-          <p className={styles.text}>
-            Правитель - это яркий лидер и тот, кто ведет за собой. Такие люди часто независимы,
-            уверенны в себе и любят брать ответственность. Прирожденный лидер и управленец.
-            Правители любят все, что связано со статусом, достижениями и результатами, не
-            стесняются показать себя во всей красе.
-          </p>
-          <p className={styles.text}>
-            В стрессе правитель становится авторитарным и даже в какой-то мере тираничным.
-            Утрачивает способность слышать других и зацикливается на собственной правоте. Может
-            быть жесток в суждениях и действиях, импульсивен и категоричен. Правители склонны
-            делить людей на своих и чужих, чувствуют превосходство на теми, кто слабее и постоянно
-            конкурирует с равными себе, но не всегда честно.
-          </p>
+          <p className={styles.text}>{firstParagraph}</p>
+          <p className={styles.text}>{secondParagraph}</p>
         </div>
       </div>
     </article>
   );
 }
 
-//ArchetypesCard.propTypes = {
+ArchetypesCard.propTypes = {
+  motivationText: PropTypes.string.isRequired,
+  fearText: PropTypes.string.isRequired,
+  strongSideText: PropTypes.string.isRequired,
+  weakSideText: PropTypes.string.isRequired,
+  strategyText: PropTypes.string.isRequired,
+  headline: PropTypes.string.isRequired,
+  openHeadline: PropTypes.string,
+  image: PropTypes.string.isRequired,
+  firstParagraph: PropTypes.string.isRequired,
+  secondParagraph: PropTypes.string.isRequired,
+};
 
-//}
+//------------------------- Example ----------------------------------------
+
+//  <ArchetypesCard
+//    motivationText="бути успішним"
+//    fearText="бути успішним"
+//    strongSideText="бути успішним"
+//    weakSideText="бути успішним"
+//    strategyText="управлять другими, через авторитет и достигать результата"
+//    headline="Правитель"
+//    openHeadline="Второй Архетип"
+//    image={Ruler}
+//    firstParagraph="Правитель - это яркий лидер и тот, кто ведет за собой. Такие люди часто независимы,
+//             уверенны в себе и любят брать ответственность. Прирожденный лидер и управленец.
+//             Правители любят все, что связано со статусом, достижениями и результатами, не
+//             стесняются показать себя во всей красе."
+//    secondParagraph="В стрессе правитель становится авторитарным и даже в какой-то мере тираничным.
+//             Утрачивает способность слышать других и зацикливается на собственной правоте. Может
+//             быть жесток в суждениях и действиях, импульсивен и категоричен. Правители склонны
+//             делить людей на своих и чужих, чувствуют превосходство на теми, кто слабее и постоянно
+//             конкурирует с равными себе, но не всегда честно."
+//  />;
