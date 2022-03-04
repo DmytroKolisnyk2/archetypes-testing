f4c19e6fab6f13a397d5825471e1d20cae1a1cad
Igoryakib <Igoryakib@gmail.com>
2022-03-04 22:21:16 +0200

    added section about

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import path from "./services/routerPath.json";
 import "./translations/i18nReact";
-// import i18n from "i18next";
-// import { useTranslation } from "react-i18next";
+import i18n from "i18next";
+import { useTranslation } from "react-i18next";
 
 import HomePage from "./pages/HomePage/HomePage";
-
+import SectionAbout from "./components/SectionAbout/SectionAbout";
+import aboutArchetypesImg from './images/aboutArchetypesImg.png';
 const App = () => {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
   return (
     <>
       {/* <div>{t("hello")}</div>
@@ -17,7 +18,8 @@ const App = () => {
       </button>
       <button onClick={() => i18n.changeLanguage("ua")} type="button">
         {t("btn.ua")}
-      </button> */}    
+      </button> */}  
+      {/* <SectionAbout title={t("SectionAboutTitle")} description={t("SectionDescription")} sectionImg={aboutArchetypesImg} />   */}
       <Routes>
         <Route path={path.home} element={<HomePage />} />
       </Routes>
