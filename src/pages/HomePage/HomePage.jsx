42616eee4a4d733c8e8d400bbc31bb0998daa11a
Igoryakib <Igoryakib@gmail.com>
2022-03-05 13:14:02 +0200

    fix i18n

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,11 +3,12 @@ import { useTranslation } from "react-i18next";
 
 import SectionAbout from '../../components/SectionAbout/SectionAbout';
 import aboutArchetypesImg from '../../images/aboutArchetypesImg.png';
+import {sectionAboutArchetypes} from '../../translations/ua/common.json';
 export default function HomePage() {
   const { t } = useTranslation();
   return (
     <main >
-      <SectionAbout title={t("SectionAboutTitle")} description={t("SectionDescription")} sectionImg={aboutArchetypesImg} />
+      <SectionAbout title={t(sectionAboutArchetypes.SectionAboutTitle)} description={t(sectionAboutArchetypes.SectionDescription)} sectionImg={aboutArchetypesImg} />
     </main>
   )
 }
