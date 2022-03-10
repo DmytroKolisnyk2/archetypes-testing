594ce9613fe5ca0a310f8512ae1349f0884083b0
unknown <you@example.com>
2022-03-10 14:04:29 +0200

    firstBlockTest

--- src/components/TestBlock/TestBlock.jsx
+++ src/components/TestBlock/TestBlock.jsx
@@ -4,29 +4,36 @@ import TestBlockStyles from "./TestBlock.module.scss"
 import Title from "../sectionTitle/sectionTitle"
 import Button from "../Button/Button"
 import SvgArrow from "../SvgArrow/SvgArrow"
+// import { firstBlock12 } from "../../services/questions/firstBlock.json";
 import { useTranslation } from "react-i18next";
 import { testBlock1 } from "../../translations/ua/common.json";
+import Block1Question from "./TestBlock1Quetion"
 
-function TestBlock ({ props }){
-  // const [count, setCount] = useState(1);
-  // setCount(2);
+function TestBlock ({props}){
+ 
   const { t } = useTranslation();
   
 
     return (
   <div>
-<Title title={'Перший блок'} 
+
+   <section>
+   <Title title={'Перший блок'} 
 text={'Виберіть найбільш підходящу вам відповідь з двох запропонованих нижче'} top={120}/>
-          {props.map((prop,index) => (
-            <>
-            <h2 className={TestBlockStyles.quetion}>{index+1}. {prop.question}</h2>
-      
-<label className={TestBlockStyles.options}><input className={TestBlockStyles.inputRadio} type="radio"name="fav_language" />{prop.options[1]} </label><br></br>
-
-<label className={TestBlockStyles.options}><input className={TestBlockStyles.inputRadio} type="radio" name="fav_language"/>{prop.options[2]}</label><br></br>
-
-            </>
+      <form action="#">
+        <ul>
+          {props.map((item, index) => (
+            <Block1Question
+              key={index}
+              number={index + 1}
+              headline={item.question}
+              options={item.options}
+            />
           ))}
+        </ul>
+      </form>
+    </section>
+  );
           <div  className={TestBlockStyles.testBlock1ButtonWrapper}>
   <Button
               width={170}
