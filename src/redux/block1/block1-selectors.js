a35595585cff2af2db84c6a0264e93a799965271
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-03-15 16:48:18 +0200

    fix bug with routes

--- src/redux/block1/block1-selectors.js
+++ src/redux/block1/block1-selectors.js
@@ -2,4 +2,7 @@ import { createSelector } from "@reduxjs/toolkit";
 
 export const getBlock1 = (state) => state.block1;
 
-export const getBlock1Completed = createSelector([getBlock1], (block) => !Boolean(block.find((item) => item.radio === null)))
+export const getBlock1Completed = createSelector([getBlock1], (block) => {
+  console.log(!Boolean(block.find((item) => item.radio === null)));
+  return !Boolean(block.find((item) => item.radio === null))
+})
--- src/redux/block2/block2-selectors.js
+++ src/redux/block2/block2-selectors.js
@@ -2,4 +2,7 @@ import { createSelector } from "@reduxjs/toolkit";
 
 export const getBlock2 = (state) => state.block2
 
-export const getBlock2Completed = createSelector([getBlock2], (block) => !Boolean(block.find((item) => item.radio === null)))
+export const getBlock2Completed = createSelector([getBlock2], (block) => {
+  console.log(!Boolean(block.find((item) => item.radio === null)));
+  return !Boolean(block.find((item) => item.radio === null))
+})
