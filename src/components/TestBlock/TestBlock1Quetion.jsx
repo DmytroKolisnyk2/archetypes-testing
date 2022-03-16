a4e08d39f85a76c589acd0948af3c2f47b163834
unknown <you@example.com>
2022-03-16 20:52:44 +0200

    testBLOCK1

--- src/components/TestBlock/TestBlock1Quetion.jsx
+++ src/components/TestBlock/TestBlock1Quetion.jsx
@@ -4,16 +4,16 @@ export default function Block2Question({ number, options, headline }) {
     const [radio, setRadio] = useState("");
     return (
       <li>
-        <h3 className={TestBlockStyles.quetion}>
+        <h3 className={TestBlockStyles.quetion}>                            
           {number}. {headline}
         </h3>
         <div>
           {options.map((item, index) => (
-      <label className={TestBlockStyles.options}><input checked={radio === index}
+      <label className={TestBlockStyles.options}><input className={TestBlockStyles.radio} checked={radio === index}
       type="radio"
       name={`block-1_${number}`}
       onChange={() => setRadio(index)}
-      id={`block-1_${number}-${index + 1}`} />{options} </label>
+      id={`block-1_${number}-${index + 1}`} />{ options } </label>
              
               
          
