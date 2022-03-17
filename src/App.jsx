60e6b4f93010499902b6784551eebfdfd52fa70d
Max Popsuy <maxim09p@gmail.com>
2022-03-17 19:33:42 +0200

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ import "./styles/App.scss";
 import GradientWrapper from "./components/GradientWrapper/GradientWrapper";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import asyncComponent from "./services/asyncComponent.js"
-import Loader from "./components/Loader/Loader"
+import asyncComponent from "./services/asyncComponent.js";
+import Loader from "./components/Loader/Loader";
 // const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
 // const TestPage = lazy(() => import("./pages/TestPage/TestPage"));
 // const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
@@ -26,6 +26,14 @@ const TestPage = asyncComponent({
   loader: () => import("./pages/TestPage/TestPage"),
   loading: Loader,
 });
+const ArchetypesPage = asyncComponent({
+  loader: () => import("./pages/ArchetypesPage/ArchetypesPage"),
+  loading: Loader,
+});
+const ArchetypeDetailPage = asyncComponent({
+  loader: () => import("./pages/ArchetypeDetailPage/ArchetypeDetailPage"),
+  loading: Loader,
+});
 // import TestPage from "./pages/TestPage/TestPage";
 
 const App = () => {
@@ -34,15 +42,61 @@ const App = () => {
       <Header></Header>
       <GradientWrapper>
         <main className="main">
-            <Routes>
-              <Route path={path.home} element={<HomePage />} />
-              <Route path={path.test} element={<TestPage />} />
-            </Routes>
+          <Routes>
+            {/* <Route path={path.home} element={<HomePage />} />
+            <Route path={path.test} element={<TestPage />} /> */}
+            <Route path={path.home} element={<HomePage />} />
+            <Route path={path.archetypes} element={<ArchetypesPage />} />
+            <Route path={path.ArchetypeDetailPage} element={<ArchetypeDetailPage />}/>
+            // <Route path={path.test} element={<TestPage />} />
+          </Routes>
         </main>
         <Footer />
       </GradientWrapper>
     </>
   );
 };
+
+// import React from "react";
+// import { Routes, Route } from "react-router-dom";
+// import path from "./services/routerPath.json";
+// import "./translations/i18nReact";
+// // import i18n from "i18next";
+// // import { useTranslation } from "react-i18next";
+
+// import "./styles/App.scss";
+
+// import HomePage from "./pages/HomePage/HomePage";
+// import ArchetypesPage from "./pages/ArchetypesPage/ArchetypesPage";
+// import ArchetypeDetailPage from "./pages/ArchetypeDetailPage/ArchetypeDetailPage";
+// import TestPage from "./pages/TestPage/TestPage";
+
+// import Header from "./components/Header/Header";
+// import Footer from "./components/Footer/Footer";
+// // import Modal from "./components/Modal/Modal.jsx";
+// import GradientWrapper from "./components/GradientWrapper/GradientWrapper";
+
+// // import { Fade, Slide } from "react-awesome-reveal";
+
+// const App = () => {
+//   return (
+//     <>
+//       <Header></Header>
+//       <GradientWrapper>
+//         <main className="main">
+//           <Routes>
+//             <Route path={path.home} element={<HomePage />} />
+//             <Route path={path.archetypes} element={<ArchetypesPage />} />
+//             <Route path={path.ArchetypeDetailPage} element={<ArchetypeDetailPage />} />
+//             <Route path={path.test} element={<TestPage />} />
+//           </Routes>
+//         </main>
+//         <Footer />
+//       </GradientWrapper>
+//     </>
+//   );
+// };
+
+// export default App;
 
 export default App;
