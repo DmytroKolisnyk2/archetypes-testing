5fa1d856926bde07fde732b4f56dbc61af7a145c
Max Popsuy <maxim09p@gmail.com>
2022-03-17 19:37:14 +0200

    nothing

---
+++ src/pages/ArchetypeDetailPage/ArchetypeDetailPage.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { useNavigate } from "react-router-dom";
+
+import styles from "./ArchetypeDetailPage.module.scss";
+import SvgArrow from "../../components/SvgArrow/SvgArrow";
+import ArchetypesCard from "../../components/ArchetypesCard/ArchetypesCard";
+import { archetypePage } from "../../translations/ua/common.json";
+
+import path from "../../services/routerPath.json";
+
+const ArchetypeDetailPage = () => {
+  const { t } = useTranslation();
+  const { archetypeId } = useParams();
+  const navigate = useNavigate();
+  const onClickBtn = () => {
+    return navigate(path.archetypes);
+  };
+  console.log(t(archetypePage[archetypeId].image));
+  return (
+    <section className={styles.section}>
+      <div className={styles.wrapperContent}>
+      <button className={styles.button} type="button" onClick={onClickBtn}>
+        <SvgArrow orientation="left" size={50} />
+      </button>
+      <ArchetypesCard
+        headline={t(archetypePage[archetypeId].title)}
+        motivationText={t(archetypePage[archetypeId].subtitle1)}
+        fearText={t(archetypePage[archetypeId].subtitle2)}
+        strongSideText={t(archetypePage[archetypeId].subtitle3)}
+        weakSideText={t(archetypePage[archetypeId].subtitle4)}
+        strategyText={t(archetypePage[archetypeId].subtitle5)}
+        firstParagraph={t(archetypePage[archetypeId].paragraph1.text)}
+        secondParagraph={t(archetypePage[archetypeId].paragraph2.text)}
+        image={t(archetypePage[archetypeId].image)}
+      />
+      </div>
+    </section>
+  );
+};
+
+export default ArchetypeDetailPage;
--- src/pages/TestPage/TestPage.jsx
+++ src/pages/TestPage/TestPage.jsx
@@ -1,13 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 
+import { useParams } from "react-router-dom";
 
-import styles from "./TestPage.module.scss";
+import { connect } from "react-redux";
+import { getBlock1Completed } from "../../redux/block1/block1-selectors";
+import { getBlock2Completed } from "../../redux/block2/block2-selectors";
+import { getBlock3Completed } from "../../redux/block3/block3-selectors";
+
 import SectionFindOut from "../../components/SectionFindOut/SectionFindOut";
+import Block2 from "../../components/Block2/Block2";
+import Block1 from "../../components/Block1/Block1";
+import Block3 from "../../components/Block3/Block3";
+import Results from "../../components/Results/Results";
+import PrivateRoute from "../../components/PrivateRoute/PrivateRoute";
+import { Fade, Slide } from "react-awesome-reveal";
 
-const TestPage = () => {
-    return(
-        <SectionFindOut />
-    );
+const TestPage = ({ block1Data, block2Data, block3Data }) => {
+  const { blockId } = useParams();
+  useEffect(() => {
+    document.querySelector(`#${blockId}`)?.scrollIntoView();
+  }, [blockId]);
+  return (
+    <>
+      {blockId !== "results" && (
+        <Fade>
+          <SectionFindOut />
+        </Fade>
+      )}
+      {blockId === "block1" && (
+        <Slide triggerOnce>
+          <Block1 />
+        </Slide>
+      )}
+      {blockId === "block2" && (
+        <PrivateRoute isCompleted={block1Data}>
+          <Slide triggerOnce>
+            <Block2 />
+          </Slide>
+        </PrivateRoute>
+      )}
+      {blockId === "block3" && (
+        <PrivateRoute isCompleted={block1Data && block2Data}>
+          <Slide triggerOnce>
+            <Block3 />
+          </Slide>
+        </PrivateRoute>
+      )}
+      {blockId === "results" && (
+        <PrivateRoute isCompleted={block1Data && block2Data && block3Data}>
+          <Slide triggerOnce>
+            <Results />
+          </Slide>
+        </PrivateRoute>
+      )}
+    </>
+  );
 };
 
-export default TestPage;
+const mapStateToProps = (state) => ({
+  block1Data: getBlock1Completed(state),
+  block2Data: getBlock2Completed(state),
+  block3Data: getBlock3Completed(state),
+});
+export default connect(mapStateToProps, null)(TestPage);
