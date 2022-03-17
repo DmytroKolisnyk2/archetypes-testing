14d917c0073397ecc533709ef4b4628f88d4c5d3
Igoryakib <Igoryakib@gmail.com>
2022-03-17 19:59:30 +0200

    added verify

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -18,8 +18,10 @@ function Results({ block1Data, block2Data, block3Data }) {
   const [thirdArchetypeId, setThirdArchetypeId] = useState([]);
   useEffect(() => {
     const archetypesCount = { ...archetypePage };
+    const listArchetypesBlock3 = { ...archetypePage };
     for (const key in archetypesCount) {
       archetypesCount[key] = 0;
+      listArchetypesBlock3[key] = 0;
     }
     block1Data
       .filter((item) => item.estimate)
@@ -31,57 +33,94 @@ function Results({ block1Data, block2Data, block3Data }) {
 
     block3Data
       .filter((item) => item.estimate)
-      .forEach((element) => (archetypesCount[element.estimate] += 5));
+      .forEach((element) => {
+        listArchetypesBlock3[element.estimate] += 5;
+        return (archetypesCount[element.estimate] += 5);
+      });
 
     // Igor => archetypesCount - це об'єкт з підрахунком по кожному архітипу
     const arrayArchetypes = Object.entries(archetypesCount);
     const sortedArray = [...arrayArchetypes].sort((a, b) => b[1] - a[1]);
-    console.log(sortedArray)
-    setFirstArchetypeId(sortedArray[0]);
-    setSecondArchetypeId(sortedArray[1]);
-    setThirdArchetypeId(sortedArray[2]);
+    const mostAmountPointsArchetypes = [
+      sortedArray[0],
+      sortedArray[1],
+      sortedArray[2],
+    ];
+    const archetypeId = mostAmountPointsArchetypes[0][1];
+    if (
+      mostAmountPointsArchetypes.filter((item) => item[1] === archetypeId)
+        .length > 1
+    ) {
+      const mostAmountPointsBlock3 = [
+        [sortedArray[0][0], listArchetypesBlock3[sortedArray[0][0]]],
+        [sortedArray[1][0], listArchetypesBlock3[sortedArray[1][0]]],
+        [sortedArray[2][0], listArchetypesBlock3[sortedArray[2][0]]],
+      ];
+      const sortedMostAmountPointsBlock3 = [...mostAmountPointsBlock3].sort(
+        (a, b) => b[1] - a[1]
+      );
+      setFirstArchetypeId(sortedMostAmountPointsBlock3[0]);
+      setSecondArchetypeId(sortedMostAmountPointsBlock3[1]);
+      setThirdArchetypeId(sortedMostAmountPointsBlock3[2]);
+    } else {
+      setFirstArchetypeId(sortedArray[0]);
+      setSecondArchetypeId(sortedArray[1]);
+      setThirdArchetypeId(sortedArray[2]);
+    }
   }, [block1Data, block2Data, block3Data]);
   return (
-    firstArchetypeId.length > 0 &&
-  <section className={styles.results}>
-    <SectionTitle title={t(resultPage.titleSection)} text={t(resultPage.subtitle)} />
-    <ArchetypesCard
-     headline={t(archetypePage[firstArchetypeId[0]].title)} 
-        motivationText={t(archetypePage[firstArchetypeId[0]].subtitle1)}
-        fearText={t(archetypePage[firstArchetypeId[0]].subtitle2)}
-        strongSideText={t(archetypePage[firstArchetypeId[0]].subtitle3)}
-        weakSideText={t(archetypePage[firstArchetypeId[0]].subtitle4)}
-        strategyText={t(archetypePage[firstArchetypeId[0]].subtitle5)}
-        firstParagraph={t(archetypePage[firstArchetypeId[0]].paragraph1.text)}
-        secondParagraph={t(archetypePage[firstArchetypeId[0]].paragraph2.text)}
-        image={t(archetypePage[firstArchetypeId[0]].image)}
-    />
+    firstArchetypeId.length > 0 && (
+      <section className={styles.results}>
+        <SectionTitle
+          title={t(resultPage.titleSection)}
+          text={t(resultPage.subtitle)}
+        />
         <ArchetypesCard
-     headline={t(archetypePage[secondArchetypeId[0]].title)} 
-        motivationText={t(archetypePage[secondArchetypeId[0]].subtitle1)}
-        fearText={t(archetypePage[secondArchetypeId[0]].subtitle2)}
-        strongSideText={t(archetypePage[secondArchetypeId[0]].subtitle3)}
-        weakSideText={t(archetypePage[secondArchetypeId[0]].subtitle4)}
-        strategyText={t(archetypePage[secondArchetypeId[0]].subtitle5)}
-        firstParagraph={t(archetypePage[secondArchetypeId[0]].paragraph1.text)}
-        secondParagraph={t(archetypePage[secondArchetypeId[0]].paragraph2.text)}
-        image={t(archetypePage[secondArchetypeId[0]].image)}
-        openHeadline={t(resultPage.titleCard1)}
-    />
+          headline={t(archetypePage[firstArchetypeId[0]].title)}
+          motivationText={t(archetypePage[firstArchetypeId[0]].subtitle1)}
+          fearText={t(archetypePage[firstArchetypeId[0]].subtitle2)}
+          strongSideText={t(archetypePage[firstArchetypeId[0]].subtitle3)}
+          weakSideText={t(archetypePage[firstArchetypeId[0]].subtitle4)}
+          strategyText={t(archetypePage[firstArchetypeId[0]].subtitle5)}
+          firstParagraph={t(archetypePage[firstArchetypeId[0]].paragraph1.text)}
+          secondParagraph={t(
+            archetypePage[firstArchetypeId[0]].paragraph2.text
+          )}
+          image={t(archetypePage[firstArchetypeId[0]].image)}
+        />
         <ArchetypesCard
-     headline={t(archetypePage[thirdArchetypeId[0]].title)} 
-        motivationText={t(archetypePage[thirdArchetypeId[0]].subtitle1)}
-        fearText={t(archetypePage[thirdArchetypeId[0]].subtitle2)}
-        strongSideText={t(archetypePage[thirdArchetypeId[0]].subtitle3)}
-        weakSideText={t(archetypePage[thirdArchetypeId[0]].subtitle4)}
-        strategyText={t(archetypePage[thirdArchetypeId[0]].subtitle5)}
-        firstParagraph={t(archetypePage[thirdArchetypeId[0]].paragraph1.text)}
-        secondParagraph={t(archetypePage[thirdArchetypeId[0]].paragraph2.text)}
-        image={t(archetypePage[thirdArchetypeId[0]].image)}
-        openHeadline={t(resultPage.titleCard2)}
-    />
-  </section>)
-  ;
+          headline={t(archetypePage[secondArchetypeId[0]].title)}
+          motivationText={t(archetypePage[secondArchetypeId[0]].subtitle1)}
+          fearText={t(archetypePage[secondArchetypeId[0]].subtitle2)}
+          strongSideText={t(archetypePage[secondArchetypeId[0]].subtitle3)}
+          weakSideText={t(archetypePage[secondArchetypeId[0]].subtitle4)}
+          strategyText={t(archetypePage[secondArchetypeId[0]].subtitle5)}
+          firstParagraph={t(
+            archetypePage[secondArchetypeId[0]].paragraph1.text
+          )}
+          secondParagraph={t(
+            archetypePage[secondArchetypeId[0]].paragraph2.text
+          )}
+          image={t(archetypePage[secondArchetypeId[0]].image)}
+          openHeadline={t(resultPage.titleCard1)}
+        />
+        <ArchetypesCard
+          headline={t(archetypePage[thirdArchetypeId[0]].title)}
+          motivationText={t(archetypePage[thirdArchetypeId[0]].subtitle1)}
+          fearText={t(archetypePage[thirdArchetypeId[0]].subtitle2)}
+          strongSideText={t(archetypePage[thirdArchetypeId[0]].subtitle3)}
+          weakSideText={t(archetypePage[thirdArchetypeId[0]].subtitle4)}
+          strategyText={t(archetypePage[thirdArchetypeId[0]].subtitle5)}
+          firstParagraph={t(archetypePage[thirdArchetypeId[0]].paragraph1.text)}
+          secondParagraph={t(
+            archetypePage[thirdArchetypeId[0]].paragraph2.text
+          )}
+          image={t(archetypePage[thirdArchetypeId[0]].image)}
+          openHeadline={t(resultPage.titleCard2)}
+        />
+      </section>
+    )
+  );
 }
 
 const mapStateToProps = (state) => ({
