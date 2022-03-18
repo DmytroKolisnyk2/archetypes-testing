495f207e27fb08cc5e7323241141c08ba482669e
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-03-18 09:30:40 +0200

    edit styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import path from "./services/routerPath.json";
 import "./translations/i18nReact";
-// import i18n from "i18next";
-// import { useTranslation } from "react-i18next";
 
 import "./styles/App.scss";
 
-// import HomePage from "./pages/HomePage/HomePage";
-// import ArchetypesPage from "./pages/ArchetypesPage/ArchetypesPage";
-// import ArchetypeDetailPage from "./pages/ArchetypeDetailPage/ArchetypeDetailPage";
-// import TestPage from "./pages/TestPage/TestPage";
-
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-// import Modal from "./components/Modal/Modal.jsx";
 import GradientWrapper from "./components/GradientWrapper/GradientWrapper";
 
-// import { Fade, Slide } from "react-awesome-reveal";
 
 import asyncComponent from "./services/asyncComponent";
 import Loader from "./components/Loader/Loader";
@@ -42,6 +33,7 @@ const ArchetypeDetailPage = asyncComponent({
 const App = () => {
   return (
     <>
+      <Loader></Loader>
       <Header></Header>
       <GradientWrapper>
         <main className="main">
