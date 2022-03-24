e718a67925fe72a99c366559d1327cd3e7089446
unknown <you@example.com>
2022-03-24 12:45:09 +0200

    footernew1.1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import path from "./services/routerPath.json";
 import "./translations/i18nReact";
-// import i18n from "i18next";
-// import { useTranslation } from "react-i18next";
-import { firstBlock } from "./services/questions/firstBlock.json";
+
+import { useSelector } from "react-redux";
+import { getLang } from "./redux/lang/lang-selector";
+import i18n from "./translations/i18nReact";
 
 import "./styles/App.scss";
 
-import HomePage from "./pages/HomePage/HomePage";
-import ArchetypesPage from "./pages/ArchetypesPage/ArchetypesPage";
-import ArchetypeDetailPage from "./pages/ArchetypeDetailPage/ArchetypeDetailPage";
-import TestPage from "./pages/TestPage/TestPage";
-
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-// import Modal from "./components/Modal/Modal.jsx";
-// import Block2 from "./components/Block2/Block2";
-import Block1 from "./components/Block1/Block1";
-// import Block3 from "./components/Block3/Block3";
 import GradientWrapper from "./components/GradientWrapper/GradientWrapper";
 
+import asyncComponent from "./services/asyncComponent";
+import Loader from "./components/Loader/Loader";
+
+const HomePage = asyncComponent({
+  loader: () => import("./pages/HomePage/HomePage"),
+  loading: Loader,
+});
+const TestPage = asyncComponent({
+  loader: () => import("./pages/TestPage/TestPage"),
+  loading: Loader,
+});
+const ArchetypesPage = asyncComponent({
+  loader: () => import("./pages/ArchetypesPage/ArchetypesPage"),
+  loading: Loader,
+});
+const ArchetypeDetailPage = asyncComponent({
+  loader: () => import("./pages/ArchetypeDetailPage/ArchetypeDetailPage"),
+  loading: Loader,
+});
+
 const App = () => {
+  const lang = useSelector(getLang);
+
+  useEffect(() => i18n.changeLanguage(lang), []);
   return (
     <>
       <Header></Header>
       <GradientWrapper>
         <main className="main">
-          {/* <Modal/> */}
           <Routes>
             <Route path={path.home} element={<HomePage />} />
             <Route path={path.archetypes} element={<ArchetypesPage />} />
             <Route path={path.ArchetypeDetailPage} element={<ArchetypeDetailPage />} />
-            <Route path={path.test} element={<TestPage />}/>
+            <Route path={path.testLink} element={<TestPage />} />
+            <Route path={path.test} element={<TestPage />} />
           </Routes>
         </main>
         <Footer />
